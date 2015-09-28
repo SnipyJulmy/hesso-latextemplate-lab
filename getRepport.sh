588b1a522c4138351e4a01d4db61e47c67929939
Sylvain Julmy <sylvain.julmy@edu.hefr.ch>
2015-09-28 12:26:11 +0200

    add script to create new report project from this repo

---
+++ getRepport.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# enable a new latex project with the $1 name
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage : getRepport.sh [rapport_name]"
+    exit
+fi
+
+name=$1
+tex=".tex"
+
+git clone git@bitbucket.org:Snipy/latexmodel_mse.git rapport
+mv rapport/base_model.tex rapport/$name$tex
+tmp="$$tmp$$"
+cat rapport/Makefile | sed -e "s/MAIN\ =\ base_model.tex/MAIN\ =\ $name$tex/" > rapport/$tmp
+mv rapport/$tmp rapport/Makefile
+rm -rf rapport/.git
+rm -rf rapport/.gitignore
